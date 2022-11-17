ef14111d68154398f2f72d44fc41def73e4ad819
Manuel1928 <manuel1998angel@gmail.com>
2022-11-17 13:05:50 -0600

    update

--- src/components/navigate/narvar.js
+++ src/components/navigate/narvar.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import { faHome, faUser, faBook, faUserGroup,faUserClock, faTools } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '../../styles/narvar.css'
 
 function Example() {
   const [show] = useState(true);
@@ -14,19 +13,7 @@ function Example() {
           <Offcanvas.Title>Menu Principal</Offcanvas.Title>
        
         <Offcanvas.Body>
-          <div>
-         <FontAwesomeIcon icon={faHome} color="blue"/>  Inicio
-         <br/>
-         <FontAwesomeIcon icon={faUser} />  Inicio
-         <br/>
-         <FontAwesomeIcon icon={faBook} />  Inicio
-         <br/>
-         <FontAwesomeIcon icon={faUserGroup} />  Inicio
-         <br/>
-         <FontAwesomeIcon icon={faUserClock} />  Inicio
-         <br/>
-         <FontAwesomeIcon icon={faTools} />  Inicio
-         <br/>
+         <div>
          </div>
         </Offcanvas.Body>
       </Offcanvas>
